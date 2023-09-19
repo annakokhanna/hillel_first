bcc804d67d4b192b3b48cdb4f61374f9e9a1d860
Anna Kokhan <a.kokhan@xmlshop.com>
2023-09-19 23:11:21 +0300

    homework 4.1

--- src/test/java/hillel/lesson4/Homework4_1.java
+++ src/test/java/hillel/lesson4/Homework4_1.java
@@ -5,10 +5,15 @@ public class Homework4_1 {
     public static void main(String[] args) {
 
         int [] numbers = {1, 2, 3, 4, 5, 6, 8, 9, 10};
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+        for (int i = 1; i < numbers.length; i++) {
 
+                if ((numbers[i] - numbers[i-1]) >1)  {
+                    System.out.println("missing number");
+                    System.out.println(numbers[i]-1);
+                }
+
+
+        }
 
     }
 }
